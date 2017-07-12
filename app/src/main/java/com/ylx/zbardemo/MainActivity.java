aa45e0c1c434ace11f079ba1665c7b10487eeaab
YangLiXiang <QNjingling1108@163.com>
2017-07-12 16:21:21 +0800

    跳转

--- app/src/main/java/com/ylx/zbardemo/MainActivity.java
+++ app/src/main/java/com/ylx/zbardemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.ylx.zbardemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ylx.zbardemo.activity.ZxingActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mZxingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mZxingBtn = (TextView) findViewById(R.id.zxing_btn);
+        initListener();
+    }
+
+    private void initListener() {
+        mZxingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ZxingActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ylx/zbardemo/activity/ZxingActivity.java
@@ -0,0 +1,15 @@
+package com.ylx.zbardemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ylx.zbardemo.R;
+
+public class ZxingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zxing);
+    }
+}
